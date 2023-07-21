07188db9452fada87a4b2930c5bdf6a86e78b465
Gabriel Araújo <gabrielaraujoc@outlook.com>
2023-07-21 13:49:55 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'api_key=2bb1ab537063f9d5a05d4f2ad436a9e8';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
